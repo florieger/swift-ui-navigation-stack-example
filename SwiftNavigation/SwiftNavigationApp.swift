2482769f6bb60806d224e2e6a66991e5c8c37f70
Florian Rieger <florian.rieger@git.appcron.com>
2024-11-29 12:53:11 +0100

    Created Xcode project.

---
+++ SwiftNavigation/SwiftNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNavigationApp.swift
+//  SwiftNavigation
+//
+//  Created by Florian Rieger on 29.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
