865aa75ef34ee3705bc6611f702b4a0cf73856ab
Florian Rieger <florian.rieger@git.appcron.com>
2024-11-29 13:45:45 +0100

    Added NavigationDestinations package.

---
+++ Packages/NavigationDestinations/Sources/NavigationDestinations/NavigationDestinations.swift
@@ -0,0 +1,11 @@
+//
+//  NavigationDestinations.swift
+//  NavigationDestinations
+//
+//  Created by Florian Rieger on 29.11.24.
+//
+
+public enum RedFeatureDestinations:Hashable {
+    case redList
+    case redDetails(itemId: Int)
+}
